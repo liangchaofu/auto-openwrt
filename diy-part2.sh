47ab11211ea2481352cf011ef1b29d795e06035e
liangchaofu <70999087+liangchaofu@users.noreply.github.com>
2020-09-09 19:08:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,13 +13,7 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-git clone https://github.com/liangchaofu/packages.git package/lcf
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/lcf
-git clone https://github.com/liangchaofu/golang.git tools
 git clone https://github.com/liangchaofu/golang.git feeds/packages/lang
 
 rm -rf openwrt/feeds/packages/net/shadowsocks-libev
 rm -rf openwrt/feeds/luci/applications/luci-app-shadowsocks-libev
-
-sed -i '$a tools-y += ucl upx' tools/Makefile
-sed -i '$a $(curdir)/upx/compile := $(curdir)/ucl/compile' tools/Makefile
