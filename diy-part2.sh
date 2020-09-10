aab8eb2b9b2e6d62281a190a4543122fc4ba22b7
liangchaofu <70999087+liangchaofu@users.noreply.github.com>
2020-09-10 14:41:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,10 +13,6 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-#git clone https://github.com/liangchaofu/golang.git lcf
-#mv lcf/golang feeds/packages/lang
-#rm -rf lcf
-
 rm -rf feeds/packages/net/shadowsocks-libev
 rm -rf feeds/luci/applications/luci-app-shadowsocks-libev
 sed -i '96,100s/^/#/g' feeds/packages/net/aria2/Makefile
