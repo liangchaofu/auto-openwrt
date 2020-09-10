6e5a68f03131f38b2d69ba0ca046723789fa976e
liangchaofu <70999087+liangchaofu@users.noreply.github.com>
2020-09-10 09:02:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,5 +23,4 @@ git clone https://github.com/liangchaofu/golang.git feeds/packages/lang
 
 rm -rf feeds/packages/net/shadowsocks-libev
 rm -rf feeds/luci/applications/luci-app-shadowsocks-libev
-feeds/packages/net/aria2/Makefile | sed '/aria2.init/d'
-feeds/packages/net/aria2/Makefile | sed '/aria2.conf/d'
+packages/net/aria2/Makefile | sed '96,100d'
